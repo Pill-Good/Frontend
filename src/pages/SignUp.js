fc2d72391b4eb675f19b0b3665079c82a16ee634
Sukyeong Park <zztnrudzz13@naver.com>
2022-08-17 00:27:21 +0900

    feat: test

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,11 +2,24 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const SignUp = () => {
-  return <SignUpWrapper>SignUp</SignUpWrapper>
+  return (
+    <SignUpWrapper>
+      <div>
+        <p className="text">hello</p>
+      </div>
+    </SignUpWrapper>
+  )
 }
 
 export default SignUp
 
 const SignUpWrapper = styled.div`
-  color: red;
+  div {
+    width: 500px;
+    height: 400px;
+    background-color: red;
+    .text {
+      color: #fff;
+    }
+  }
 `
