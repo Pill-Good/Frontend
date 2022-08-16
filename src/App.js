3f2efbd835abb391ad3c49bbc700d3fd277fefe7
Sukyeong Park <zztnrudzz13@naver.com>
2022-08-17 00:11:41 +0900

    feat: initial setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Main from './pages/Main'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const SignUp = () => {
+  return <SignUpWrapper>SignUp</SignUpWrapper>
+}
+
+export default SignUp
+
+const SignUpWrapper = styled.div`
+  color: red;
+`
